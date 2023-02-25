8d5b86c19f18cfe75ee6e459b3177c829c6e3c96
CyberBow <rizksalimrizk@gmail.com>
2023-02-25 23:34:52 +0200

    3 buttons

--- 3/buttons.py
+++ 3/buttons.py
@@ -2,4 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+
+def myClick():
+    myLabel = Label(root, text="CLicked")
+    myLabel.pack()
+
+
+myButton = Button(root, text="Click Me!", command=myClick )
+myButton.pack()
+
 root.mainloop()
